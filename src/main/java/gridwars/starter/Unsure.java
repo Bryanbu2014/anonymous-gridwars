171aed2dad3fef5e5ef9d06596ebbfca0ff2f467
Oh Wei Liang <wei.oh@stud.th-deg.de>
2024-03-13 15:08:38 +0100

    clean up code

--- src/main/java/gridwars/starter/Unsure.java
+++ src/main/java/gridwars/starter/Unsure.java
@@ -16,7 +16,7 @@ import static java.lang.Math.floor;
  * Simple bot that expands into all directions if there is a cell that does not belong to the bot
  */
 
-public class Unsure implements PlayerBot {
+public class CheckerboardBotV4 implements PlayerBot {
     Coordinates initialCoordinates;
 
     int state = 0;
@@ -32,67 +32,71 @@ public class Unsure implements PlayerBot {
 
         double populationSoftLimit;
         // Soft limit ensure that we get maximum benefit from the growth rate
-        populationSoftLimit = 1.0; //0.600625
+        populationSoftLimit = 0.600625; //0.600625
 
         if (universeView.getCurrentTurn() > 60) {
             this.state = 1;
-            populationSoftLimit = 0.600625;
+            //populationSoftLimit = 0.600625;
         }
 
-        if (this.state == 1){
-            for (Coordinates cell : myCells) {
-                int currentPopulation = universeView.getPopulation(cell);
-                // splits population into 4 equal parts
-                int split = 4;
-                int usablePopulation = currentPopulation - 5;
-                boolean NearbyEnemyUp = false;
-                boolean NearbyEnemyDown = false;
-                boolean NearbyEnemyLeft = false;
-                boolean NearbyEnemyRight = false;
-                boolean Outer = false;
-                boolean Inner = false;
-                boolean corner = false;
+        for (Coordinates cell : myCells) {
+            int currentPopulation = universeView.getPopulation(cell);
+            // splits population into 4 equal parts
+            int split = 4;
+            int usablePopulation = currentPopulation - 4;
+            boolean NearbyEnemyUp = false;
+            boolean NearbyEnemyDown = false;
+            boolean NearbyEnemyLeft = false;
+            boolean NearbyEnemyRight = false;
+            boolean Outer = false;
+            boolean Inner = false;
+            boolean corner = false;
 
-                boolean FacingRightUp = false;
-                boolean FacingRightDown = false;
-                boolean FacingLeftUp = false;
-                boolean FacingLeftDown = false;
-                boolean center = false;
-                boolean OnTheLeft = false;
-                boolean OnTheRight = false;
-                boolean OnTop = false;
-                boolean OnBottom = false;
-
+            boolean FacingRightUp = false;
+            boolean FacingRightDown = false;
+            boolean FacingLeftUp = false;
+            boolean FacingLeftDown = false;
+            boolean center = false;
+            boolean OnTheLeft = false;
+            boolean OnTheRight = false;
+            boolean OnTop = false;
+            boolean OnBottom = false;
 
 
-                double attackSplit = 1.79;
 
-                Random rand = new Random();
-                int rand_direction = rand.nextInt(4);
+            double attackSplit = 1.79;
 
-                List< MovementCommand.Direction> outerDirections = new ArrayList<>();
-                List< MovementCommand.Direction> innerDirections = new ArrayList<>();
+            Random rand = new Random();
+            int rand_direction = rand.nextInt(4);
+
+            List< MovementCommand.Direction> outerDirections = new ArrayList<>();
+            List< MovementCommand.Direction> innerDirections = new ArrayList<>();
+            int outerSplit = 0;
+            int innerSplit = 0;
 
 
-                if ((cell.getX() == initialCoordinates.getX()) && (cell.getY() == initialCoordinates.getY())) {
-                    center = true;
-                }else if ((cell.getX() > initialCoordinates.getX()) && (cell.getY() > initialCoordinates.getY())){
-                    FacingRightDown = true;
-                } else if ((cell.getX() > initialCoordinates.getX()) && (cell.getY() < initialCoordinates.getY())){
-                    FacingRightUp = true;
-                } else if ((cell.getX() < initialCoordinates.getX()) && (cell.getY() > initialCoordinates.getY())){
-                    FacingLeftDown = true;
-                } else if ((cell.getX() < initialCoordinates.getX()) && (cell.getY() < initialCoordinates.getY())){
-                    FacingLeftUp = true;
-                } else if ((cell.getX() == initialCoordinates.getX()) && (cell.getY() < initialCoordinates.getY())) {
-                    OnTop = true;
-                } else if ((cell.getX() == initialCoordinates.getX()) && (cell.getY() > initialCoordinates.getY())) {
-                    OnBottom = true;
-                } else if ((cell.getX() > initialCoordinates.getX()) && (cell.getY() == initialCoordinates.getY())) {
-                    OnTheRight = true;
-                } else if ((cell.getX() < initialCoordinates.getX()) && (cell.getY() == initialCoordinates.getY())) {
-                    OnTheLeft = true;
-                }
+            if ((cell.getX() == initialCoordinates.getX()) && (cell.getY() == initialCoordinates.getY())) {
+                center = true;
+            }else if ((cell.getX() > initialCoordinates.getX()) && (cell.getY() > initialCoordinates.getY())){
+                FacingRightDown = true;
+            } else if ((cell.getX() > initialCoordinates.getX()) && (cell.getY() < initialCoordinates.getY())){
+                FacingRightUp = true;
+            } else if ((cell.getX() < initialCoordinates.getX()) && (cell.getY() > initialCoordinates.getY())){
+                FacingLeftDown = true;
+            } else if ((cell.getX() < initialCoordinates.getX()) && (cell.getY() < initialCoordinates.getY())){
+                FacingLeftUp = true;
+            } else if ((cell.getX() == initialCoordinates.getX()) && (cell.getY() < initialCoordinates.getY())) {
+                OnTop = true;
+            } else if ((cell.getX() == initialCoordinates.getX()) && (cell.getY() > initialCoordinates.getY())) {
+                OnBottom = true;
+            } else if ((cell.getX() > initialCoordinates.getX()) && (cell.getY() == initialCoordinates.getY())) {
+                OnTheRight = true;
+            } else if ((cell.getX() < initialCoordinates.getX()) && (cell.getY() == initialCoordinates.getY())) {
+                OnTheLeft = true;
+            }
+            if (this.state == 1){
+
+
                 // if the population of the current box exceeds 20 (2.0/0.1), we enter a phase that splits our population to expand the territory
                 if (usablePopulation > (populationSoftLimit / (universeView.getGrowthRate() - 1))) {
                     // this loop looks for enemies within a certain distance, if an enemy box is found, it breaks out of the loop.
@@ -130,8 +134,6 @@ public class Unsure implements PlayerBot {
                         //commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, 47));
                         //commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, 47));
                     } else {
-                        int outerSplit = 0;
-                        int innerSplit = 0;
 
                         // Check left, right, up, down for cells that don't belong to me
                         for (MovementCommand.Direction direction : MovementCommand.Direction.values()) {
@@ -154,24 +156,39 @@ public class Unsure implements PlayerBot {
                             Outer = true;
                         }
                         // Expand
-                        if (Inner && (currentPopulation == universeView.getMaximumPopulation())) {
-                            switch (rand_direction) {
-                                case 0:
-                                    commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation));
-                                    break;
-                                case 1:
-                                    commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation));
-                                    break;
-                                case 2:
-                                    commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation));
-                                    break;
-                                case 3:
-                                    commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation));
-                                    break;
-                            }
-                        } else if (Inner) {
-                            for (MovementCommand.Direction direction : innerDirections) {
-                                commandList.add(new MovementCommand(cell, direction, usablePopulation / innerSplit));
+                        if (Inner) {
+                            if (FacingLeftUp) {
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation / 2));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation / 2));
+                            } else if (FacingLeftDown) {
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation / 2));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation / 2));
+                            } else if (FacingRightUp) {
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation / 2));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation / 2));
+                            } else if (FacingRightDown) {
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation / 2));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation / 2));
+                            } else if (center) {
+                                for (MovementCommand.Direction direction : innerDirections) {
+                                    commandList.add(new MovementCommand(cell, direction, usablePopulation / innerSplit));
+                                }
+                            } else if (OnTop) {
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation / 3));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation / 3));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation / 3));
+                            } else if (OnBottom) {
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation / 3));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation /3));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation / 3));
+                            } else if (OnTheLeft) {
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation / 3));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation / 3));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation /3));
+                            } else if (OnTheRight) {
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation /3));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation / 3));
+                                commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation / 3));
                             }
                         } else if (Outer){
                             for (MovementCommand.Direction direction : outerDirections) {
@@ -186,56 +203,7 @@ public class Unsure implements PlayerBot {
 
                     }
                 }
-            }
-        } else {
-            for(Coordinates cell : myCells) {
-                int currentPopulation = universeView.getPopulation(cell);
-                // splits population into 4 equal parts
-                int split = 4;
-                int usablePopulation = currentPopulation - 4;
-                boolean NearbyEnemyUp = false;
-                boolean NearbyEnemyDown = false;
-                boolean NearbyEnemyLeft = false;
-                boolean NearbyEnemyRight = false;
-                boolean Outer = false;
-                boolean Inner = false;
-                boolean corner = false;
-
-                boolean FacingRightUp = false;
-                boolean FacingRightDown = false;
-                boolean FacingLeftUp = false;
-                boolean FacingLeftDown = false;
-                boolean center = false;
-                boolean OnTheLeft = false;
-                boolean OnTheRight = false;
-                boolean OnTop = false;
-                boolean OnBottom = false;
-
-                if ((cell.getX() == initialCoordinates.getX()) && (cell.getY() == initialCoordinates.getY())) {
-                    center = true;
-                }else if ((cell.getX() > initialCoordinates.getX()) && (cell.getY() > initialCoordinates.getY())){
-                    FacingRightDown = true;
-                } else if ((cell.getX() > initialCoordinates.getX()) && (cell.getY() < initialCoordinates.getY())){
-                    FacingRightUp = true;
-                } else if ((cell.getX() < initialCoordinates.getX()) && (cell.getY() > initialCoordinates.getY())){
-                    FacingLeftDown = true;
-                } else if ((cell.getX() < initialCoordinates.getX()) && (cell.getY() < initialCoordinates.getY())){
-                    FacingLeftUp = true;
-                } else if ((cell.getX() == initialCoordinates.getX()) && (cell.getY() < initialCoordinates.getY())) {
-                    OnTop = true;
-                } else if ((cell.getX() == initialCoordinates.getX()) && (cell.getY() > initialCoordinates.getY())) {
-                    OnBottom = true;
-                } else if ((cell.getX() > initialCoordinates.getX()) && (cell.getY() == initialCoordinates.getY())) {
-                    OnTheRight = true;
-                } else if ((cell.getX() < initialCoordinates.getX()) && (cell.getY() == initialCoordinates.getY())) {
-                    OnTheLeft = true;
-                }
-
-                List< MovementCommand.Direction> outerDirections = new ArrayList<>();
-                List< MovementCommand.Direction> innerDirections = new ArrayList<>();
-
-                int outerSplit = 0;
-                int innerSplit = 0;
+            } else {
                 if (usablePopulation > (populationSoftLimit / (universeView.getGrowthRate() - 1))) {
                     // Check left, right, up, down for cells that don't belong to me
                     for (MovementCommand.Direction direction : MovementCommand.Direction.values()) {
@@ -276,21 +244,21 @@ public class Unsure implements PlayerBot {
                                 commandList.add(new MovementCommand(cell, direction, usablePopulation / innerSplit));
                             }
                         } else if (OnTop) {
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation / 3));
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation / 3));
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation / 3));
+                            //commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation / 3));
+                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation ));
+                            //commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation / 3));
                         } else if (OnBottom) {
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation / 3));
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation / 3));
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation / 3));
+                            //commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation / 3));
+                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation ));
+                            //commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation / 3));
                         } else if (OnTheLeft) {
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation / 3));
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation / 3));
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation / 3));
+                            //commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation / 3));
+                            //commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation / 3));
+                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.LEFT, usablePopulation ));
                         } else if (OnTheRight) {
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation / 3));
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation / 3));
-                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation / 3));
+                            commandList.add(new MovementCommand(cell, MovementCommand.Direction.RIGHT, usablePopulation ));
+                            //commandList.add(new MovementCommand(cell, MovementCommand.Direction.UP, usablePopulation / 3));
+                            //commandList.add(new MovementCommand(cell, MovementCommand.Direction.DOWN, usablePopulation / 3));
                         }
                     } else if (Outer) {
                         for (MovementCommand.Direction direction : outerDirections) {
@@ -305,8 +273,6 @@ public class Unsure implements PlayerBot {
                             commandList.add(new MovementCommand(cell, direction, usablePopulation / 4));
                         }
                     }
-
-
                 }
 
             }
