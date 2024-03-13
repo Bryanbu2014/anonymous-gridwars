06564fef20e30a51c369ef4933a51214390063c5
Bryan Bu <wen.bu@stud.th-deg.de>
2024-03-14 00:32:48 +0100

    Upload Lapsap.java

--- src/main/java/gridwars/starter/EmulatorRunner.java
+++ src/main/java/gridwars/starter/EmulatorRunner.java
@@ -2,22 +2,21 @@ package gridwars.starter;
 
 import cern.ais.gridwars.Emulator;
 
-
 /**
  * Instantiates the example bots and starts the game emulator.
  */
 public class EmulatorRunner {
 
     public static void main(String[] args) {
-        //MovingBot blueBot = new MovingBot();
-        //ExpandBot blueBot = new ExpandBot();
+        // MovingBot blueBot = new MovingBot();
+        // ExpandBot blueBot = new ExpandBot();
 
-        //TestBot blueBot = new TestBot();
-        //CheckerboardBotV2 redBot = new CheckerboardBotV2();
-        //CheckerboardBotV3 blueBot = new CheckerboardBotV3();
-        //CheckerboardBotV3 redBot = new CheckerboardBotV3();
-        CheckerboardBotV4 redBot = new CheckerboardBotV4();
-        Checkerboard blueBot = new Checkerboard();
+        // TestBot blueBot = new TestBot();
+        // CheckerboardBotV2 redBot = new CheckerboardBotV2();
+        // CheckerboardBotV3 blueBot = new CheckerboardBotV3();
+        // CheckerboardBotV3 redBot = new CheckerboardBotV3();
+        UnsureV2 blueBot = new UnsureV2();
+        Unsure redBot = new Unsure();
 
         Emulator.playMatch(blueBot, redBot);
     }
